d18c16226c73fd6a1a930452fe8b1d66cd95dc3d
Carlosygaby <191144185+Carlosygaby@users.noreply.github.com>
2025-03-03 21:40:53 +0100

    Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronombres = ["La", "Nuestra"];
+let adjetivos = ["pequeña", "gran"];
+let objeto = ["casa", "estafa"];
+
+const generarDominio = (numero) => {
+  let listaDominios = "";
+  for (let i = 0; i < numero; i++) {
+    let dominio =
+      pronombres[Math.floor(Math.random() * pronombres.length)] +
+      adjetivos[Math.floor(Math.random() * adjetivos.length)] +
+      objeto[Math.floor(Math.random() * objeto.length)];
+
+    listaDominios += `<li>${dominio}.com</li>`;
+  }
+  document.getElementById("lista").innerHTML = listaDominios;
+};
+
+window.onload = function () {
+  generarDominio(8);
 };
